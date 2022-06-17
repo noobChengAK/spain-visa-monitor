dd542c2039699f9046e3182e62148b6fcd46c90d
ChengAK <74144331+noobChengAK@users.noreply.github.com>
2022-06-18 00:17:15 +0100

    Yao li yun
    Yao li yun

--- utils/config.py
+++ utils/config.py
@@ -6,6 +6,6 @@ CENTER = ('England', 'Manchester', 'Normal', 'Tourism')
 # CENTER = ('Scotland', 'Edinburgh', 'Normal', 'Tourism')
 # CENTER = ('Bristol', 'London', 'Normal', 'Tourist')
 # =============== PERSONAL CONFIG FOR VISA ===============
-OPENED_PAGE = 'https://uk.blsspainvisa.com/visa4spain/book-date/QwertYU123'  # your BLS appointment page link
-EMAIL = '123@456.com'  # BLS email
-PASSWORD = 'BLSpassword'  # BLS password
+OPENED_PAGE = 'https://uk.blsspainvisa.com/visa4spain/book-appointment/X6WppJShhg'  # your BLS appointment page link
+EMAIL = 'qcshs715@gmail.com'  # BLS email
+PASSWORD = '0715Karroy///'  # BLS password
